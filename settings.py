2f7c9ae115cca4f47356b5c304e4d0534ee97665
max180692 <111618832+max180692@users.noreply.github.com>
2022-08-26 13:10:49 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,4 @@
+API_TOKEN = 'API TOKEN TELEGRAM'
+HEADERS = {'accept':'*/*','user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+URL = 'https://www.gismeteo.ru'
+URL_API = 'https://www.gismeteo.ru/api/v2/search/searchresultforsuggest/'
